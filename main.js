ab6aaf3e55902d2a9b8995faf0a501919a0f68c4
David Thong Nguyen <dev@davidtn.com>
2020-11-11 13:43:04 -0500

    Clean up

--- main.js
+++ main.js
@@ -1,62 +1,3 @@
-/**
-d3.json("./highest-grossing-per-region.json").then((raw) => {
-  let dataset = Object.keys(raw).reduce((accumulator, currentVal) => {
-    let regions = [];
-    Object.keys(raw[currentVal]).forEach((x) => {
-      regions.push({ region: x, data: raw[currentVal][x] });
-    });
-    accumulator.push({ year: +[currentVal], regions });
-    return accumulator;
-  }, []);
-  let nested = d3
-    .nest()
-    .key((x) => x.year)
-    .entries(dataset);
-  console.log(nested);
-
-  // ********************************************************************* //
-  // ******************************* SLIDER ****************************** //
-  // ********************************************************************* //
-  var slider = d3
-    .sliderHorizontal()
-    .min(d3.min(dataset, (y) => y.year))
-    .max(d3.max(dataset, (y) => y.year))
-    .step(1)
-    .width(300)
-    .displayValue(true)
-    .on("onchange", (val) => {
-      d3.select("#value").text(val);
-      d3.select("#result")
-        .selectAll("div")
-        .data(
-          nested
-            .filter((x) => +[x.key] === val)
-            .map((x) => {
-              console.log(x.values[0]);
-              return x;
-            })
-        )
-        .enter()
-        .append("div")
-        .text((el, i) => {
-          return JSON.stringify(el.values);
-        });
-    });
-
-  d3.select("#slider")
-    .append("svg")
-    .attr("width", 500)
-    .attr("height", 100)
-    .append("g")
-    .attr("transform", "translate(30,30)")
-    .call(slider);
-
-  // ********************************************************************* //
-  // ******************************* CHART ******************************* //
-  // ********************************************************************* //
-  var svg = d3.select("svg");
-});
- */
 let width =
   window.innerWidth > window.innerHeight
     ? window.innerHeight
@@ -318,6 +259,8 @@ let zoom = (d) => {
       ]);
       return (t) => zoomTo(i(t));
     });
+
+  // display game details at game level
 
   animate();
 };
